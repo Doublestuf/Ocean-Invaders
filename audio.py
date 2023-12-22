d24de4984f6ceb7ac9e86380ee3e101ec5380187
Doublestuf <153161997+Doublestuf@users.noreply.github.com>
2023-12-21 19:29:32 -0500

    changed select sound

--- audio.py
+++ audio.py
@@ -5,6 +5,8 @@ hurt_sound = pg.mixer.Sound("audio/hurt.wav")
 select_sound = pg.mixer.Sound("audio/select.wav")
 hit_sound = pg.mixer.Sound("audio/hit.wav")
 
+select_sound.set_volume(0.25)
+
 pg.mixer_music.load("audio/SeeingDouble.wav")
 pg.mixer_music.set_volume(0.5)
 pg.mixer_music.play(loops=-1)
